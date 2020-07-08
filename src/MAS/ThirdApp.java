808ad2fe0f7f98ab8670e936314b0ce4048a62ee
Andrew <mikandpost@gmail.com>
2020-07-08 20:28:32 +0300

    Выполненная третья домашка (ИСПРАВЛЕННАЯ)

--- src/MAS/ThirdApp.java
+++ src/MAS/ThirdApp.java
@@ -3,25 +3,28 @@ import java.util.Random;
 import java.util.Scanner;
 public class ThirdApp {
     public static void main(String[] args) {
-        int guessedNumber = getRandomNumber();
-        //System.out.println(guessedNumber);
-        System.out.println("Я загадал число от 0 до 9, у тебя есть 3 попытки, чтобы его угадать");
-        System.out.println("-------------------------------------------------------------------");
-        for (int i = 0; i < 3; i++) {
-        int gamerNumber = getNumberFromGamer("Введите число в пределах от 0 до 9", 0, 9);
-            if (gamerNumber == guessedNumber) {
-                System.out.println("Верно!");
-                break;
-            } else if (2-i == 0){
+        int go;
+        do {
+            int guessedNumber = getRandomNumber();
+            System.out.println(guessedNumber);
+            System.out.println("Я загадал число от 0 до 9, у тебя есть 3 попытки, чтобы его угадать");
+            System.out.println("-------------------------------------------------------------------");
+            for (int i = 0; i < 3; i++) {
+                int gamerNumber = getNumberFromGamer("Введите число в пределах от 0 до 9", 0, 9);
+                if (gamerNumber == guessedNumber) {
+                    System.out.println("Верно!");
+                    break;
+                } else if (2 - i == 0) {
                     System.out.println("Ты проиграл, у тебя закончились все попытки(");
                 } else {
-                System.out.println("Не угадал! Давай попробуем еще раз!");
-                System.out.println("У тебя еще осталось " + (2 - i) + " попытки(а)");
-                System.out.println("----------------------------------------------");
-                System.out.println();
+                    System.out.println("Не угадал! Давай попробуем еще раз!");
+                    System.out.println("У тебя еще осталось " + (2 - i) + " попытки(а)");
+                    System.out.println("----------------------------------------------");
+                    System.out.println();
+                }
             }
-        }
-
+            go = repeat();
+        } while (go == 1);
     }
     static int getRandomNumber() {
         Random rand = new Random();
@@ -37,7 +40,15 @@ public class ThirdApp {
         } while (x < min || x > max);
         return x;
     }
-
+    private static int repeat() {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        do {
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+            x = sc.nextInt();
+        } while (x > 1 || x < 0);
+        return x;
+    }
 }
 
 
