93bad22d447dd2e2eaf046b34e1a58483c799505
Andrew <mikandpost@gmail.com>
2020-07-20 12:46:41 +0300

    Выполненная шестая домашка

---
+++ src/MAS/Animal.java
@@ -0,0 +1,14 @@
+package MAS;
+
+public class Animal
+{
+    protected int run;
+    protected int swim;
+    protected double jump;
+
+    Animal(int run, double jump, int swim) {
+        this.run = run;
+        this.jump = jump;
+        this.swim = swim;
+    }
+}
